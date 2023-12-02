cec30317f040758679fba5266f330eb779967e1f
Lisa Perez <lisa.perez@insa-lyon.fr>
2023-12-02 22:04:44 +0100

    addition partie C dans string.h

--- string.h
+++ string.h
@@ -22,11 +22,28 @@ class string{
     string& operator=(char c);
     //string& operator+(const string& s, const char* c);
 
+    //LISA
+    //Constructor et Destructor :
+    string(const char* s); //Constructor
+    ~string(); //destructor
+
+    //Fonctions membres :
+    int capacity() const;
+    bool empty() const;
+    void reserve(int new_capacity);
+    void afficher() const;
+    int get_allocated_memory() const; //Accesor for allocated_memory
+
+    //Operators :
+    string& operator=(const char* s); //Operator=
+    string operator+(const string&); //Operator+
+
 
   protected:
     //char* str_[SIZE];
     char* str_;
     int taille_ ;
+    int allocated_memory_ = 15;
 
 
 };
